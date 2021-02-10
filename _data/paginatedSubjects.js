35d3079ff5734c16f91d6e513ee66aee1a8ad1df
Jim O'Donnell <jim@zooniverse.org>
2021-02-10 18:46:41 +0000

    Hack together some subject indexes

---
+++ _data/paginatedSubjects.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+function pageSubjects(groupSubjects) {
+  const pageSize = 50
+  const subjectPages = [];
+  const { group } = groupSubjects[0];
+  const pageCount = Math.ceil(groupSubjects.length / pageSize);
+  for (let i = 0; i < pageCount; i++) {
+    const start = i * pageSize;
+    const finish = start + pageSize;
+    const pageSubjects = groupSubjects.slice(start, finish)
+    subjectPages.push({
+      groupID: group.zooniverse_id,
+      groupName: group.name,
+      page: i,
+      subjects: pageSubjects
+    });
+  }
+  return subjectPages
+}
+
+module.exports = function paginatedSubjects() {
+  let groupedSubjects = [];
+  fs.readdirSync('./_data/subjects').forEach(file => {
+    const groupID = file.split('.')[0];
+    const groupSubjects = require(`./subjects/${file}`);
+    groupedSubjects = groupedSubjects.concat(pageSubjects(groupSubjects));
+  });
+  return groupedSubjects;
+}
