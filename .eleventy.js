8a66a44224088b6bfbc0be27346d689a66a42215
Jim O'Donnell <jim@zooniverse.org>
2021-02-10 12:11:23 +0000

    Basic default config and layout

---
+++ .eleventy.js
@@ -0,0 +1,24 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addLayoutAlias("default", "layouts/default.njk");
+
+  return {
+    templateFormats: [
+      "md",
+      "njk",
+      "html",
+      "liquid"
+    ],
+
+    markdownTemplateEngine: "liquid",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+
+    // These are all optional, defaults are shown:
+    dir: {
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    }
+  };
+}
