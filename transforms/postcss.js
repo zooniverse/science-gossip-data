8a668e808a01e371846a503fa01cecfbc34ec832
Jim O'Donnell <jim@zooniverse.org>
2021-12-09 10:12:19 +0000

    Add postCSS with Sass support (#4)
    * Install postCSS and plugins
    
    * Add a postcss transform
    Parse Sass and minify the output.
    
    * Sassify the CSS
    
    * Refactor main.njk
    Move styles into main.scss.

---
+++ transforms/postcss.js
@@ -0,0 +1,21 @@
+const
+  postcss = require('postcss'),
+  postcssPlugins = [
+    require('postcss-advanced-variables'),
+    require('postcss-nested'),
+    require('cssnano')
+  ],
+  postcssOptions = {
+    from: 'site/scss/entry.scss',
+    syntax: require('postcss-scss')
+  };
+
+module.exports = async function (content) {
+
+  if (!this.outputPath.endsWith('.css')) return content;
+
+  return (
+    await postcss(postcssPlugins).process(content, postcssOptions)
+  ).css;
+
+};
